7e2fa2a411d62d1594bf76443519b52422815db0
AldoRS <aldopablo7@hotmail.com>
2021-02-26 16:30:05 +0000

    se agregaron comentarios para una mejor visualización del código.

--- main.cpp
+++ main.cpp
@@ -9,24 +9,27 @@ using std :: cout;
 using std :: cin;
 using std :: endl;
 
-#define _CANTEST 2
+#define _CANTEST 2 //Defino una constante 
 
-typedef char str10[10];
+typedef char str10[10];//Variables de tipo char para los nombres, apellidos y carrera de cada estudiante
 typedef char str15[20];
 typedef char str20[30];
 
-struct tRegEstudiante{
+struct tRegEstudiante{ //Registro de un estudante
   str10 nombre;
   str15 apellido;
   str20 carrera;
 
 }; 
 
-typedef tRegEstudiante tVecEst[_CANTEST];
+typedef tRegEstudiante tVecEst[_CANTEST];//Array de registros
 
+//Prototipos
 void inicializarVecEst(tVecEst, int);
 void ingresarEst(tVecEst, int);
 void mostrarEst(tVecEst, int);
+
+//Funcion principal
 int main() {
 
   tVecEst v;
@@ -38,6 +41,8 @@ int main() {
   mostrarEst(v, cant);
 
 }
+
+//Funciones y procedimientos
 void inicializarVecEst(tVecEst v, int cant){
   for(int i = 0; i < cant; i++){
     strcpy(v[i].nombre, "");
